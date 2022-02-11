0c26760eb6a6d8e5fc3d2e4f93a42fd775de8a79
Rachel <rachelreillydev@gmail.com>
2022-02-11 13:41:38 -0600

    sets up basic boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Svelte/Tailwind/TypeScript Boilerplate
+
+This repo handles the setup for a basic Svelte project with TypeScript and TailwindCSS. The setup was primarily taken from [Rahul Barwal's dev.to post](https://dev.to/rahulbarwal/svelte-typescript-tailwind-2njo).
---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  console.log('This is my boilerplate')
+</script>
+
+<main class="flex flex-col items-center h-screen">
+  <h1>Hello Svelte/Tailwind/TypeScript boilerplate!</h1>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
